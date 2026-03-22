4b272a6b42c24a5643f94fbcf391b864fb3f2977
Hyun Gu Kang <khg5022@gmail.com>
2026-03-22 11:51:50 +0100

    Set active adapters for BERT model

--- esupar/supar/modules/pretrained.py
+++ esupar/supar/modules/pretrained.py
@@ -62,6 +62,7 @@ class TransformerEmbedding(nn.Module):
                                    set_active=True,
                                    with_head=False,
                                    use_safetensors=False)
+            self.bert.set_active_adapters(adapter_name)
         else:
             from transformers import AutoModel
             self.bert = AutoModel.from_pretrained(
